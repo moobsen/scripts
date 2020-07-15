d6dcdd889e94191143bcc9a39d0558f65f65aa14
Moritz <mo@bluebirdmountain.de>
2020-07-15 11:46:27 +0200

    added simple meditation timer (python script, needs vlc)

---
+++ meditate/meditate.py
@@ -0,0 +1,14 @@
+# sound from https://freesound.org/people/surly/sounds/91196/
+
+import os
+import time
+
+MEDITATION_DURATION = 5 #in minutes
+file = 'meditation_bell.mp3'
+
+time.sleep(20)
+os.system("vlc --play-and-exit " + file)
+time.sleep(60*MEDITATION_DURATION)
+os.system("vlc --play-and-exit " + file)
+
+
