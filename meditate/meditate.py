6e62aa288d5f1cf2b065dda9a6c17460058682c9
Moritz <mo@bluebirdmountain.de>
2020-10-24 21:36:43 +0200

    meditate.py: small changes

--- meditate/meditate.py
+++ meditate/meditate.py
@@ -3,12 +3,15 @@
 import os
 import time
 
-MEDITATION_DURATION = 5 #in minutes
+MEDITATION_DURATION = 10 #in minutes
 file = 'meditation_bell.mp3'
 
+print("Meditation starting in 20 seconds")
 time.sleep(20)
+print("MEditation Starting")
 os.system("vlc --play-and-exit " + file)
 time.sleep(60*MEDITATION_DURATION)
 os.system("vlc --play-and-exit " + file)
+print("Meditation ended")
 
 
