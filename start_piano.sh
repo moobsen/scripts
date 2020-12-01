0b2148d33e76530a2d8a936f9411308876f69fb7
Moritz <mo@bluebirdmountain.de>
2020-12-01 10:16:44 +0100

    trying to find channel automatically

--- start_piano.sh
+++ start_piano.sh
@@ -5,4 +5,7 @@ aconnect -i
 echo "--------- Lising MIDI Outputs ------------"
 aconnect -o
 # conncet my piano to qsynth
-aconnect 20: 128:
+#aconnect 20: 128:
+PIANO_CHANNEL=$(aconnect -i | grep "Impact LX61+" | grep client | cut -d" " -f2)
+FLUID_CHANNEL=$(aconnect -o | grep "FLUID Synth" | grep client | cut -d" " -f2)
+aconnect $PIANO_CHANNEL $FLUID_CHANNEL
