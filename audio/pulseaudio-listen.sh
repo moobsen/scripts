814d6f8aa061d9e8c79bcbc71e8644179dc6fdde
Moritz Obermeier <mo@bluebirdmountain.de>
2022-07-11 18:50:56 +0200

    adding script to stream pulse audio output

---
+++ audio/pulseaudio-listen.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pactl load-module module-rtp-recv latency_msec=50 
---
+++ audio/pulseaudio-stream-to-ip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+$IP=$1
+pactl load-module module-null-sink sink_name=rtp
+pactl load-module module-rtp-send source=rtp.monitor destination_ip=$IP
+# you still need to select the correct interface as output. for me it is called "Null Output"
