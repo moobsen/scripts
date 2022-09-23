a55a0ca6e83a2c84c72616d57a3d247549a5922a
Moritz Obermeier <mo@bluebirdmountain.de>
2022-09-23 11:42:45 +0200

    workin on keyboard color script

--- keyboard-color/match-screen-color.sh
+++ keyboard-color/match-screen-color.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
 scrot -f screenshot.png
 RGB_COLOR=$(convert screenshot.png -resize 1x1\! \
 	-format "%[fx:int(255*r+.5)],%[fx:int(255*g+.5)],%[fx:int(255*b+.5)]" info:-)
--- keyboard-color/set-color.sh
+++ keyboard-color/set-color.sh
@@ -1,2 +1,4 @@
+# e.g.>  ./set-color.sh 0xFF0000 to set keyboard to red
+# 0xRRGGBB
 #!/bin/bash
 echo -n $1 > /sys/devices/platform/tuxedo_keyboard/color_left
