6c563d48ab65da0b547d7e016096be961904877d
Moritz Obermeier <moritz@comydo.com>
2022-11-11 14:11:22 +0100

    updating display scripts

--- displays/all-displays-thinkpad-home.sh
+++ displays/all-displays-thinkpad-home.sh
@@ -2,6 +2,7 @@
 xrandr --auto
 xrandr --output DP-2 --rotate left
 xrandr --output HDMI-1 --above eDP-1
-xrandr --output DP-2 --right-of HDMI-1
+xrandr --output DP-2 --left-of HDMI-1
 xrandr --output HDMI-1 --primary
+feh --bg-scale /home/mo/Pictures/fractal.png
 
--- displays/all-displays-thinkpad-office.sh
+++ displays/all-displays-thinkpad-office.sh
@@ -4,4 +4,6 @@ xrandr --output DP-1-8 --right-of eDP-1
 xrandr --output DP-1-1 --right-of DP-1-8
 xrandr --output DP-1-1 --rotate left
 xrandr --output DP-1-8 --primary
+feh --bg-scale /home/mo/Pictures/fractal.png
+
 
