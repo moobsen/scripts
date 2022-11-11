7af02311b108a57ecd9f8d311bcc8a7df2fd10bd
Moritz Obermeier <moritz@comydo.com>
2022-11-11 14:10:58 +0100

    creating pulse audio streaming scripts just in time for pulse audio deprecation

--- audio/pulseaudio-stream-to-ip.sh
+++ audio/pulseaudio-stream-to-ip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-$IP=$1
+IP=$1
 pactl load-module module-null-sink sink_name=rtp
 pactl load-module module-rtp-send source=rtp.monitor destination_ip=$IP
 # you still need to select the correct interface as output. for me it is called "Null Output"
