5539f2896d72ae78b4ab16fdca466c344acdb9c7
Moritz Obermeier <mo@bluebirdmountain.de>
2023-10-18 22:14:18 +0200

    updating screens

--- displays/all-displays-tuxedo-home.sh
+++ displays/all-displays-tuxedo-home.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+DISPLAY_LEFT="DisplayPort-1"
+DISPLAY_TOP="DisplayPort-0"
+
+
 xrandr --auto
-xrandr --output DisplayPort-0 --above eDP
-xrandr --output DisplayPort-1 --left-of eDP
+xrandr --output $DISPLAY_LEFT --rotate left
+xrandr --output $DISPLAY_TOP --rotate normal
+xrandr --output $DISPLAY_TOP --above eDP
+xrandr --output $DISPLAY_LEFT --left-of  $DISPLAY_TOP
+
