543fc5545320338f50d7c176bc3d6643544b5c7b
Moritz Obermeier <moritz@comydo.com>
2024-10-02 13:59:01 +0200

    adding new display setup

--- displays/all-displays-thinkpad-T470-home.sh
+++ displays/all-displays-thinkpad-T470-home.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-LEFT_ROTATED="DP-1"
-TOP="HDMI-2"
+LEFT_ROTATED="HDMI-2"
+TOP="DP-1"
 
 xrandr --auto
 xrandr --output $LEFT_ROTATED --rotate left
 xrandr --output $TOP --above eDP-1
 xrandr --output $LEFT_ROTATED --left-of $TOP
 xrandr --output $TOP --primary
-i3-msg 'rename workspace 2 to 4'
-i3-msg 'rename workspace 3 to 2'
-i3-msg 'rename workspace 1 to 3'
-i3-msg 'rename workspace 4 to 1'
+i3-msg 'rename workspace 1 to 4'
+i3-msg 'rename workspace 3 to 1'
+i3-msg 'rename workspace 4 to 3'
 feh --bg-scale  /home/mo/Pictures/solar_punk.jpg
